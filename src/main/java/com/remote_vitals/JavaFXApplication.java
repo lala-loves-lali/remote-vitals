8604be33dec55bb066310941504283d4c7660966
Sheikh-Muneeb-Ahmed <183858883+muneebahmed-nust@users.noreply.github.com>
2025-05-02 14:24:15 +0500

    spring errors removed

--- src/main/java/com/remote_vitals/JavaFXApplication.java
+++ src/main/java/com/remote_vitals/JavaFXApplication.java
@@ -1,30 +1,43 @@
 package com.remote_vitals;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
 
+/**
+ * JavaFX Application entry point
+ * This is a standalone JavaFX application for testing
+ */
 public class JavaFXApplication extends Application {
 
-    
+
+    private ConfigurableApplicationContext context;
 
     @Override
+    public void init() throws Exception {
+        context = org.springframework.boot.SpringApplication.run(RemoteVitalsApplication.class);
+    }
+    @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(JavaFXApplication.class.getResource("/sign-up-page.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Remote Vitals Monitoring System");
+        // Load the FXML file
+        FXMLLoader fxmlLoader = new FXMLLoader(JavaFXApplication.class.getResource("/simple.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+        // Configure the stage
+        stage.setTitle("JavaFX Test Application");
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * Main method to launch the application
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
-
-        launch();
+        launch(args);
     }
 }
--- src/main/java/com/remote_vitals/Test.java
+++ src/main/java/com/remote_vitals/Test.java
@@ -1,29 +1,29 @@
-package com.remote_vitals;
-
-import com.remote_vitals.user.entities.Patient;
-import com.remote_vitals.user.enums.Gender;
-import com.remote_vitals.user.repositories.PatientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-public class Test {
-    public PatientRepository patientRepository;
-
-    @Autowired
-    public void PatientRepository(PatientRepository patientRepository){
-        this.patientRepository = patientRepository;
-    }
-
-    public void tester(){
-        Patient patient = new Patient();
-        patient.setFirstName("John");
-        patient.setLastName("Smith");
-        patient.setBloodGroup("B+");
-        patient.setGender(Gender.MALE);
-        patient.setEmail("example@email.com");
-        patient.setPassword("12345");
-        this.patientRepository.save(patient);
-       // System.out.println(this.patientRepository.findByEmail("example@email.com").toString());
-    }
-}
+//package com.remote_vitals;
+//
+//import com.remote_vitals.user.entities.Patient;
+//import com.remote_vitals.user.enums.Gender;
+//import com.remote_vitals.user.repositories.PatientRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class Test {
+//    public PatientRepository patientRepository;
+//
+//    @Autowired
+//    public void PatientRepository(PatientRepository patientRepository){
+//        this.patientRepository = patientRepository;
+//    }
+//
+//    public void tester(){
+//        Patient patient = new Patient();
+//        patient.setFirstName("John");
+//        patient.setLastName("Smith");
+//        patient.setBloodGroup("B+");
+//        patient.setGender(Gender.MALE);
+//        patient.setEmail("example@email.com");
+//        patient.setPassword("12345");
+//        this.patientRepository.save(patient);
+//       // System.out.println(this.patientRepository.findByEmail("example@email.com").toString());
+//    }
+//}
