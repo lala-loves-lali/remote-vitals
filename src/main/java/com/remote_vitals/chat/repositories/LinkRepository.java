7c62ab55770decb791e5163eca81bec89979ec41
Taimoor Ashraf <lala.loves.lali2@gmail.com>
2025-05-04 16:17:07 -0700

    Almost All DB Side Done

--- src/main/java/com/remote_vitals/chat/repositories/LinkRepository.java
+++
@@ -1,7 +0,0 @@
-package com.remote_vitals.chat.repositories;
-
-import com.remote_vitals.chat.entities.Link;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LinkRepository extends JpaRepository<Link, Integer> {
-}
